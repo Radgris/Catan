81b675b20c9c324361d522b61e3ab7c369903c8d
Radgris <golden_run@hotmail.com>
2017-11-21 11:50:08 -0700

    basic functionality towards the board
    kinda working

---
+++ Catan/Assets/Map.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexprefab;
+
+	int heigth = 5;
+
+	int width = 5;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < heigth; y++) {
+
+				Instantiate (hexprefab, new Vector3 (x*5, 0, y*5), Quaternion.identity);
+
+				
+
+
+			}
+		}
+			
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
